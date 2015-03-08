79fc0dec5e506ac6446a44e22c379b3fa9f29a05
yubin <yubin@yahoo-inc.com>
2015-03-07 22:42:09 -0800

    add employee

---
+++ app/adapters/employee.js
@@ -0,0 +1,36 @@
+import Adapter from './base';
+import Serializer from '../serializers/employee';
+import Ember from 'ember';
+
+var isEmpty = Ember.isEmpty;
+
+export default Adapter.extend({
+  serializer: Serializer.create(),
+
+  findQuery: function (store, type, query) {
+    var url = this.buildURL();
+    // Get on id = 2 or id = 3
+    var ids = query.ids;
+    var onStr = '';
+    if (!isEmpty(ids)) {
+      if (ids.length && ids.length > 1) { // TBD Array
+        onStr = ids.map(function(i){return 'id='+i.id;}).join(' or ');
+      } else { // only one
+        onStr = 'id=' + ids.id;
+      }
+    } else { // Get All!
+
+    }
+
+    onStr = onStr ? 'ON ' + onStr : '';
+    console.log(onStr);
+
+    var requestStr = 'GET ' + onStr;
+    return this.ajax(url, 'POST', {
+      data: {requestString: requestStr},
+      serviceAppName: 'UserAppInfo',
+      userAppId: 'Fl2GDgDECXcbmJsBAJVayUhuLwkAAAA'
+    });
+  }
+
+});
---
+++ app/models/employee.js
@@ -0,0 +1,18 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  user_id: DS.attr('number'),
+  first: DS.attr('string'),
+  last: DS.attr('string'),
+  email: DS.attr('string'),
+  type: DS.attr('string'),
+  from: DS.attr('string'),
+  report_to_id: DS.attr('number'),
+  installed_app: DS.attr('string'),
+  active: DS.attr('boolean'),
+
+  didLoad: function(){
+    setInterval(function() {self.reload();console.log('employee reload')}, 10*1000); //every 10s
+  }
+});
